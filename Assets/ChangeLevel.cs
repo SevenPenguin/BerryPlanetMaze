0528f069476a74bbabdce814e7ecc6dd6f6086b7
ahm-ko <ahmet@kokulu.net>
2023-04-07 17:29:46 -0400

    SFX on new level menu

--- Assets/ChangeLevel.cs
+++ Assets/ChangeLevel.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeLevel : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
+    private AudioSource levelComplete;
+
+    private void Start()
+    {
+        levelComplete = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        menu.SetActive(true);
+        levelComplete.Play();
     }
 }
