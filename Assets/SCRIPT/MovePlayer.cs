91310a66097b707bde93ba7dc1ee1fd181e5df98
ahm-ko <ahmet@kokulu.net>
2023-04-07 16:50:07 -0400

    Update MovePlayer.cs

--- Assets/SCRIPT/MovePlayer.cs
+++ Assets/SCRIPT/MovePlayer.cs
@@ -18,6 +18,7 @@ public class MovePlayer : MonoBehaviour
     void Start()
     {
         walkSound = GetComponent<AudioSource>();
+        walkSoundEffect = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
