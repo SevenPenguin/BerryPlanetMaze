eaee64d8f3c6cba007a542d3f66b302807d8e218
pineapplelimejelly <eva.a.y.tsai@gmail.com>
2023-04-07 23:04:29 -0400

    Configured next level menu.

--- Assets/NextLevelMenu.cs
+++ Assets/NextLevelMenu.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NextLevelMenu : MonoBehaviour
 {
+
+    public GameObject nextlevelMenuUI;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -15,4 +19,21 @@ public class NextLevelMenu : MonoBehaviour
     {
         
     }
+
+    public void StartNextLevel()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Player has quit the game.");
+        Application.Quit();
+    }
 }
+
